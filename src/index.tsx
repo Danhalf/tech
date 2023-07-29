17c5103f203addaa3a791723f0f930782367808d
Danhalf <danhel@rocketmail.com>
2023-07-29 09:43:20 +0300

    Added new CRA project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './_metronic/assets/fonticon/fonticon.css'
+import './_metronic/assets/keenicons/duotone/style.css'
+import './_metronic/assets/keenicons/outline/style.css'
+import './_metronic/assets/keenicons/solid/style.css'
+import './_metronic/assets/sass/style.scss'
+import './_metronic/assets/sass/plugins.scss'
+import './_metronic/assets/sass/style.react.scss'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
