8d3cce48318477251068e0cd190b6a0a4c055589
danhalf <danhel@rocketmail.com>
2023-11-02 20:13:22 +0200

    fixed: last page render

--- src/components/dashboard/helpers/pagination/renderPagination.tsx
+++ src/components/dashboard/helpers/pagination/renderPagination.tsx
@@ -6,54 +6,74 @@ import {
     useQueryResponseLoading,
 } from 'common/core/QueryResponseProvider';
 import { useQueryRequest } from 'common/core/QueryRequestProvider';
-import { getLocalState, initialQueryState } from '_metronic/helpers';
-import { UsersListType } from 'common/interfaces/UserData';
+import { getLocalState } from '_metronic/helpers';
+import { UsersListType, UsersType } from 'common/interfaces/UserData';
 import { LOC_STORAGE_USER_STATE } from 'common/app-consts';
+import { getTotalUsersRecords } from 'components/dashboard/users/user.service';
 
 interface UsersListPaginationProps {
     list: UsersListType;
-    totalRecords: number;
 }
 
 const { login, usersPage } = getLocalState();
 
-export const UsersListPagination = ({ list, totalRecords }: UsersListPaginationProps) => {
-    const [currentPage, setCurrentPage] = useState<number>(initialQueryState.currentpage);
+export const UsersListPagination = ({ list }: UsersListPaginationProps) => {
+    const { state, updateState } = useQueryRequest();
+    const [currentPage, setCurrentPage] = useState<number>(state.currentpage);
     const isLoading = useQueryResponseLoading(list);
     const listLength = useQueryResponseDataLength(list);
-    const [pagesCount, setPagesCount] = useState<number>(totalRecords);
+    const [totalRecords, setTotalRecords] = useState<number>(0);
     const [pageNumbers, setPageNumbers] = useState<number[]>([]);
+    const [totalPages, setTotalPages] = useState<number>(0);
     const showedPages = 3;
 
-    const { state, updateState } = useQueryRequest();
+    useEffect(() => {
+        const fetchTotalRecords = async () => {
+            const totalList = list === UsersType.ACTIVE ? 'list' : 'listdeleted';
+            const { total } = await getTotalUsersRecords(totalList);
+            setTotalRecords(total);
+        };
+        fetchTotalRecords();
+        const getPages = async (): Promise<number> => {
+            const res = await Math.ceil(totalRecords / count);
+            return res;
+        };
+        getPages().then((res) => {
+            updatePageNumbers(res);
+            setTotalPages(res);
+        });
+    }, [totalRecords, currentPage]);
 
-    const recordsPerPage = initialQueryState.count;
+    const { count } = state;
+
+    const updatePageNumbers = (length: number): void => {
+        setPageNumbers(Array.from({ length }, (_, index) => index));
+    };
 
     useEffect(() => {
-        setPageNumbers(Array.from({ length: totalPages }, (_, index) => index));
-        if (usersPage) {
-            handleSetCurrentPage(usersPage);
+        updatePageNumbers(totalPages);
+        const page = getLocalState().usersPage;
+        if (page) {
+            handleSetCurrentPage(page);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [usersPage]);
+    }, [usersPage, totalRecords]);
 
     useEffect(() => {
         if (!listLength) {
             currentPage > 0 && handleSetCurrentPage(currentPage - 1);
-            pagesCount && setPagesCount((prev) => prev - 1);
-            setPageNumbers(Array.from({ length: totalPages }, (_, index) => index));
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [listLength]);
 
     useEffect(() => {
         if (!!state.search?.length) {
-            setPagesCount(listLength);
+            setTotalRecords(listLength);
         } else {
-            setPagesCount(totalRecords);
+            setTotalRecords(totalRecords);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [listLength, state.search, recordsPerPage, currentPage]);
+    }, [listLength, state.search, currentPage]);
 
     const handleSetCurrentPage = (page: number): void => {
         setCurrentPage(page);
@@ -61,8 +81,6 @@ export const UsersListPagination = ({ list, totalRecords }: UsersListPaginationP
 
         updateState({ ...state, currentpage: page });
     };
-
-    const totalPages = Math.ceil(pagesCount / recordsPerPage);
 
     return (
         <div className='w-100 py-6 col-sm-12 col-md-7 d-flex align-items-center justify-content-center'>
@@ -152,7 +170,8 @@ export const UsersListPagination = ({ list, totalRecords }: UsersListPaginationP
                 </ul>
 
                 <div className='mt-4 text-center fs-5'>Current page: {currentPage + 1}</div>
-                <div className='mt-4 text-center fs-5'>Records per page: {recordsPerPage}</div>
+                <div className='mt-4 text-center fs-5'>Total pages: {totalPages}</div>
+                <div className='mt-4 text-center fs-5'>Records per page: {count}</div>
                 <div className='mt-4 text-center fs-5'>Total records: {totalRecords}</div>
             </div>
         </div>
