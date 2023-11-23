eefc4a6628810cdc0b7f71beabdcf89898f3d4a9
Danhalf <danhel@rocketmail.com>
2023-11-23 08:46:12 +0200

    added: api into common services

--- src/components/dashboard/common/common.service.ts
+++ src/components/dashboard/common/common.service.ts
@@ -1,10 +1,3 @@
-import {
-    Microservice,
-    MicroserviceCounters,
-    MicroserviceServerData,
-} from 'common/interfaces/MicroserviceServerData';
-import { ServiceCheckDBResponse, ServiceStopResponse } from 'common/interfaces/ActionStatus';
-import { ServicesSortParams } from 'common/interfaces/QueriesParams';
 import { fetchApiData } from 'common/api/fetchAPI';
 
 export const getImportList = (useruid?: string): Promise<any> => {
@@ -16,4 +9,28 @@ export const deleteImportItem = (itemuid: string): Promise<any> => {
 };
 export const getImportItemInfo = (itemuid: string): Promise<any> => {
     return fetchApiData<any>('GET', `import/${itemuid}/metadata`);
+};
+
+export const getTemplateReports = (): Promise<any> => {
+    return fetchApiData<any>('GET', ` reports/list`);
+};
+
+export const deleteReportsItem = (itemuid: string): Promise<any> => {
+    return fetchApiData<any>('POST', `reports/${itemuid}/deleteitem`);
+};
+
+export const setReportsItemInfo = (itemuid: string): Promise<any> => {
+    return fetchApiData<any>('POST', `reports/${itemuid}/set`);
+};
+
+export const getTemplatePrints = (): Promise<any> => {
+    return fetchApiData<any>('GET', ` print/list`);
+};
+
+export const deletePrintItem = (itemuid: string): Promise<any> => {
+    return fetchApiData<any>('POST', `print/${itemuid}/deleteitem`);
+};
+
+export const setPrintItemInfo = (itemuid: string): Promise<any> => {
+    return fetchApiData<any>('POST', `print/${itemuid}/set`);
 };
